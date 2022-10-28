26ca4dbbe638cb2ecc6557788c29e334641d178d
santosh214 <santoshsingh74827@gmail.com>
2022-10-28 11:45:16 +0530

    done routong

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { TodoComponent } from "./todo/todo.component";
+import { ChildComponent } from "./child/child.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "todo",
+    component: TodoComponent,
+  },
+  {
+    path: "child",
+    component: ChildComponent,
+  },
+  {
+    path: "user/:id",
+    component: UserDetailComponent,
+  },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
